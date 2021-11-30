4aa03fd94d070f5cc547771a8d48719531ff14d0
Denis C <denischirchir@gmail.com>
2021-11-30 13:59:32 +0300

    change GitClass

--- src/main/java/tech/getarrays/employeemanager/monitoring/GitClass.java
+++ src/main/java/tech/getarrays/employeemanager/monitoring/GitClass.java
@@ -1,9 +1,11 @@
 package tech.getarrays.employeemanager.monitoring;
 
 public class GitClass {
-    public GitClass(String method) {
+    public GitClass(String method, Integer num) {
         this.method = method;
+        this.num = num;
     }
 
     public String method;
+    public Integer num;
 }
