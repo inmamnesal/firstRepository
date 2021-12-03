53b71dc85dc0437ef651002242690d2e23961366
Denis C <denischirchir@gmail.com>
2021-12-03 18:47:19 +0300

    user1 change file

--- src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -2,7 +2,9 @@ package tech.getarrays.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class EmployeemanagerApplication {
 
