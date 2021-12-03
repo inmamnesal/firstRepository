532edccf80f294989d744d308c48d3924fd2e3a0
Denis C <denischirchir@gmail.com>
2021-12-03 17:25:45 +0300

    add class EmployeeController

---
+++ src/main/java/tech/getarrays/employeemanager/controller/EmployeeController.java
@@ -0,0 +1,7 @@
+package tech.getarrays.employeemanager.controller;
+
+public class EmployeeController {
+    public static void main(String[] args) {
+        System.out.println("Hello from add-feature1");
+    }
+}
