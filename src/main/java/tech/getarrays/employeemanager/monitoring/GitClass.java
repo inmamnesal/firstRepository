4aa6677f5ce7ddfeafb22b913fc7b81eed94bd3e
Denis C <denischirchir@gmail.com>
2021-12-05 13:18:50 +0300

    change GitClass

--- src/main/java/tech/getarrays/employeemanager/monitoring/GitClass.java
+++ src/main/java/tech/getarrays/employeemanager/monitoring/GitClass.java
@@ -4,6 +4,8 @@ public class GitClass {
     public GitClass(String method, Integer num) {
         this.method = method;
         this.num = num;
+        System.out.println("f1");
+        //modify from branch add-feature1
     }
 
     public String method;
